327be9181f12bb84d9f0cf6449417e70365f4fda
Julian Cheal <julian.cheal@oracle.com>
2015-09-23 19:11:37 +0100

    OMG So many adds, mainly Bower So imagine you want to mange your assets but not with Rails Then Bower is your friend.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,13 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# application.js, application.css, and all non-JS/CSS in app/assets
+# folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# Bower install assests into vendor/assets which is not default for Rails
+Rails.application.config.assets.compile = true
+Rails.application.config.assets.paths <<
+  Rails.root.join('vendor', 'assets', 'bower_components')
+# Precompile Bootstrap fonts
+Rails.application.config.assets.precompile <<
+  %r{bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
